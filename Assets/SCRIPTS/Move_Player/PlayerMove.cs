ee160f352d2c11bcf6d3a8194e70f231f8d73b29
Christopher Alysson <chrisalysson29@gmail.com>
2025-12-16 04:31:33 -0300

    correção movimentação e camera

--- Assets/SCRIPTS/Move_Player/PlayerMove.cs
+++ Assets/SCRIPTS/Move_Player/PlayerMove.cs
@@ -3,18 +3,29 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 5f;
+    public float speed = 2f;
     public float gravity = -9.81f;
     public float jumpHeight = 2f;
     public float sensibilidadeX = 2f;
     float rotY = 0f;
+    
 
     Vector3 velocity;
 
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 4f;
+        }
+        else
+        {
+            speed = 2f;
+        }
+
+
+            float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
