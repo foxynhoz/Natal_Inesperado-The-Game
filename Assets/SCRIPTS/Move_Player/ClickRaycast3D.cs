6ed88e4ff0ccba2e0c5608bf00ff8a4bff589b54
Christopher Alysson <chrisalysson29@gmail.com>
2025-12-16 18:58:05 -0300

    Update ClickRaycast3D.cs

--- Assets/SCRIPTS/Move_Player/ClickRaycast3D.cs
+++ Assets/SCRIPTS/Move_Player/ClickRaycast3D.cs
@@ -21,15 +21,17 @@ public class ClickRaycast3D : MonoBehaviour
                 if (hit.collider.CompareTag("Toys")) //Task Pegar brinquedos
                 {
                     Destroy(hit.collider.gameObject);
-
+                    Task.remainingToys--;
                 }  
                 if(hit.collider.CompareTag("Trash")) //Task Jogar lixo no lixo
                 {
                     Destroy(hit.collider.gameObject);
+                    Task.remainingTrash--;
                 }
                 if(hit.collider.CompareTag("Dishes")) //Task Pratos
                 {
                     Destroy(hit.collider.gameObject);
+                    Task.remainingDishes--;
                 }
 
             }
@@ -37,3 +39,6 @@ public class ClickRaycast3D : MonoBehaviour
         }
     }
 }
+
+// https://youtube.com/shorts/xT5meeMTv6I?si=l0verlFAd8Fmdp6f sound effect toy 
+
